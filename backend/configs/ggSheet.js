e009310d9660c4493da5bebe46971217b2e2d8bf
Hieu <21520235@gm.uit.edu.vn>
2025-03-26 20:02:10 +0700

    Remove .env

--- backend/configs/ggSheet.js
+++ backend/configs/ggSheet.js
@@ -3,12 +3,13 @@ const { google } = require("googleapis");
 const credentials = require("../configKeyGGSheet.json");
 
 
+
 const auth = new google.auth.GoogleAuth({
     credentials,
     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
   });
 
-const sheetId = "1bNZM6Z4U0yc0C8KsQyFXevrdZqkgvvrPgWgsDedBkk0";
+const sheetId = process.env.SHEET_ID;
 const sheets = google.sheets({ version: "v4", auth });
 
 module.exports = { sheets, sheetId };
